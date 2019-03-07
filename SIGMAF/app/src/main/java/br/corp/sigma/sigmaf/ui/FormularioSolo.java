c4a6817bb98c1ac34c8fa27710205cf76c2baa31
AdrianoSena <adrsena@live.com>
2019-03-07 02:03:39 -0300

    Adicionada a barra de menu com o botão de salvar

--- SIGMAF/app/src/main/java/br/corp/sigma/sigmaf/ui/FormularioSolo.java
+++ SIGMAF/app/src/main/java/br/corp/sigma/sigmaf/ui/FormularioSolo.java
@@ -2,6 +2,7 @@ package br.corp.sigma.sigmaf.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import br.corp.sigma.sigmaf.R;
 
@@ -11,5 +12,11 @@ public class FormularioSolo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_solo);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Preenchendo o conteudo do menu com um XML
+        getMenuInflater().inflate(R.menu.menu_formulario_ponto,menu); //inflando o menu com o xml
+        return super.onCreateOptionsMenu(menu);
     }
 }
